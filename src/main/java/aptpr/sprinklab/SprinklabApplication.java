49d8dc7cfb0b833bccec270e66e2f3f5c771c5ad
gtr <qwpomnzx5555@gmail.com>
2024-10-22 07:52:59 +0300

    first commit

---
+++ src/main/java/aptpr/sprinklab/SprinklabApplication.java
@@ -0,0 +1,13 @@
+package aptpr.sprinklab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinklabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinklabApplication.class, args);
+    }
+
+}
