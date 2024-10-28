d1ac0df0d2f5cd5d4a4844f20b834ef9c740455f
mmuopm <mulyish@gmail.com>
2024-10-28 11:07:49 +0300

    12

--- src/main/java/Proj/laba/model/BookRentInfo.java
+++ src/main/java/Proj/laba/model/BookRentInfo.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "book_rent_info")
 @Getter
@@ -16,8 +18,25 @@ import lombok.Setter;
 , sequenceName = "book_rent_info_seq", allocationSize = 1)
 
 public class BookRentInfo extends GenericModel {
+    @ManyToOne (cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "FK_RENT_BOOK_INFO_USER"))
+    private User user;
 
     @ManyToOne (cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn
+    @JoinColumn(name = "book_id", nullable = false,
+            foreignKey = @ForeignKey(name = "FK_RENT_BOOK_INFO_BOOK"))
+    private Book book;
 
+    @Column(name = "rent_date", nullable = false)
+    private LocalDateTime rentDate;
+
+    @Column(name = "return_date", nullable = false)
+    private LocalDateTime returnDate;
+
+    @Column(name = "rent_period", nullable = false)
+    private Integer rentPeriod;
+
+    @Column(name = "is_returned", nullable = false)
+    private boolean isReturned;
 }
