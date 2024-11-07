9bc6efcbf1c46af7f8893a36d0aad6ab3fa3de02
mmuopm <mulyish@gmail.com>
2024-11-07 13:22:26 +0300

    MEOOOOWWW

--- src/main/java/Proj/library/model/Genre.java
+++ src/main/java/Proj/library/model/Genre.java
@@ -1,10 +1,18 @@
 package Proj.library.model;
 
 public enum Genre {
-    FANTASY,
-    HORROR,
-    DRAMA,
-    SCI_FI,
-    COMEDY,
-    ADVENTURE
+    FANTASY("Фантастика"),
+    SCIENCE_FICTION("Научная Фантастика"),
+    DRAMA("Драма"),
+    NOVEL("Роман");
+
+    private final String genreTextDisplay;
+
+    Genre(String text) {
+        this.genreTextDisplay = text;
+    }
+
+    public String getGenreTextDisplay() {
+        return genreTextDisplay;
+    }
 }
