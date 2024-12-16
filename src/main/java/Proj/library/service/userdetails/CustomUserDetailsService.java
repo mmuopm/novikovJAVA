f1b527f1d76a3cfc354a5070f9aa39e0d26f8259
mmuopm <mulyish@gmail.com>
2024-12-16 11:56:14 +0300

    meoww

--- src/main/java/Proj/library/service/userdetails/CustomUserDetailsService.java
+++ src/main/java/Proj/library/service/userdetails/CustomUserDetailsService.java
@@ -2,11 +2,11 @@ package Proj.library.service.userdetails;
 
 import Proj.library.constants.UserRolesConstants;
 import Proj.library.model.Role;
+import Proj.library.model.User;
 import Proj.library.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
