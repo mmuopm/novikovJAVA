cf39f78a10ad797a8a33ef1f911c6eb8047d711c
mmuopm <mulyish@gmail.com>
2024-12-20 08:37:57 +0300

    mmeow

--- src/main/java/Proj/library/config/jwt/JWTTokenFilter.java
+++ src/main/java/Proj/library/config/jwt/JWTTokenFilter.java
@@ -1,4 +1,57 @@
 package Proj.library.config.jwt;
 
-public class JWTTokenFilter {
+import Proj.library.service.userdetails.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.net.http.HttpHeaders;
+
+public class JWTTokenFilter
+        extends OncePerRequestFilter {
+
+    private final JWTTokenUtil jwtTokenUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JWTTokenFilter(JWTTokenUtil jwtTokenUtil, CustomUserDetailsService customUserDetailsService) {
+        this.jwtTokenUtil=jwtTokenUtil;
+        this.customUserDetailsService=customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IDException {
+        String token = null;
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header== null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        token = header.split(" ")[1].trim();
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(jwtTokenUtil.getUsernameFromToken(token));
+
+        if (!jwtTokenUtil.validateToken(token, userDetails)) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
+
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request,response);
+    }
 }
