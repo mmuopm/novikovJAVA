762bdc8d7a5f353e4ca9020409c7a3e831f1a58c
mmuopm <mulyish@gmail.com>
2024-12-26 15:37:54 +0300

    мяу

--- src/main/java/Proj/library/controllers/rest/UserController.java
+++ src/main/java/Proj/library/controllers/rest/UserController.java
@@ -8,23 +8,25 @@ import Proj.library.model.User;
 import Proj.library.service.GenericService;
 import Proj.library.service.UserService;
 import Proj.library.service.userdetails.CustomUserDetailsService;
+import groovy.util.logging.Slf4j;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
+@SecurityRequirement(name = "Bearer Authentication")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Tag(name = "Пользователи",
         description = "Контроллер для работы с пользователями библиотеки")
 public class UserController
